6fc8373da672a903dac3d9deb575cf21ac6ed5e5
bulutoztemur <bulutoztemur@hotmail.com>
2019-04-17 22:38:45 +0300

    'profile'

--- application/__init__.py
+++ application/__init__.py
@@ -11,10 +11,10 @@ import os
 
 
 app = Flask(__name__)
-S_KEY = os.environ.get('SEC_KEY', None)
-app.config['SECRET_KEY'] = S_KEY
-DB_URL = os.environ.get('DATABASE_URL', None)
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+'''S_KEY = os.environ.get('SEC_KEY', None)'''
+app.config['SECRET_KEY'] = '\x1f\xeb\xda\xbe\xbd1\xd8\xda\xcf\xc5\xa6e]vQ_k.\xe0\x82\x9a\xea\xe6\xc1'
+'''DB_URL = os.environ.get('DATABASE_URL', None)'''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ghxubndgrsqqbw:27815973445b0f3b9f42607322204077c5f9fc6408b19430c6240c79afacd769@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/d5up03ss7quea6'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- application/forms.py
+++ application/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from application.models import User
@@ -23,8 +25,8 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That email is taken. Please choose a different one')
 
-    def validate_mobile_phone(self, mobilephone):
-        user = User.query.filter_by(mobile_phone=mobilephone.data).first()
+    def validate_mobile_phone(self, mobile_phone):
+        user = User.query.filter_by(mobile_phone=mobile_phone.data).first()
         if user:
             raise ValidationError('That mobile phone number is taken. Please choose a different one')
 
@@ -34,4 +36,34 @@ class LoginForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
     remember = BooleanField('Remember Me')
+
+
+class UpdateAccountForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=60)])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    mobile_phone = StringField('Mobile Phone', validators=[DataRequired(), Length(min=6, max=20)])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+    def validate_mobile_phone(self, mobile_phone):
+        if mobile_phone.data != current_user.mobile_phone:
+            user = User.query.filter_by(mobile_phone=mobile_phone.data).first()
+            if user:
+                raise ValidationError('That mobile phone number is taken. Please choose a different one')
+
 
